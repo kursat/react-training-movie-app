fc3bf355f01a252f6ce87ba7f33b9509b797a4e0
Kürşat Yiğitoğlu <kursat.yigitoglu@gmail.com>
2024-03-02 10:48:14 +0300

    redux persist config

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
 import NewApp from './NewApp';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
-            {/*<NewApp />*/}
+            <PersistGate persistor={persistor}>
+                <App />
+                {/*<NewApp />*/}
+            </PersistGate>
         </Provider>
     </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,25 @@
-import { combineSlices, configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { counterSlice } from './features/counterSlice';
 import { usersApi } from './api/usersApi';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-const rootReducer = combineSlices(counterSlice);
+const persistConfig = {
+    key: 'root',
+    storage,
+};
 
-// const rootReducer2 = combineReducers({
-//     counter: counterSlice.reducer,
-//     counter2: counterSlice.reducer,
-// });
+const rootReducer = combineReducers({
+    [counterSlice.reducerPath]: counterSlice.reducer,
+    [usersApi.reducerPath]: usersApi.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        [counterSlice.reducerPath]: counterSlice.reducer,
-        [usersApi.reducerPath]: usersApi.reducer,
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(usersApi.middleware),
 });
+
+export const persistor = persistStore(store);
